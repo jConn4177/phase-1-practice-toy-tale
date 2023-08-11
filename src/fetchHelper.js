fc5196791d9400bce33a3eecab4abbac4a18a143
Jason <jconn99801@gmail.com>
2023-08-11 09:09:38 -0800

    testing git extension

---
+++ src/fetchHelper.js
@@ -0,0 +1,34 @@
+const getJSON = (url) => {
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      throw response.statusText;
+    }
+    return response.json();
+  });
+};
+
+const postJSON = (url, data) => {
+  return fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (!response.ok) {
+      throw response.statusText;
+    }
+    return response.json();
+  });
+};
+
+const patchJSON = (url, data) => {
+  return fetch(url, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (!response.ok) {
+      throw response.statusText;
+    }
+    return response.json();
+  });
+};
